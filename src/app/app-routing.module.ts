6e437bc9607959628a53ff7f515ffed561b4f088
dstegner <dmstegner@gmail.com>
2020-08-16 13:23:24 -0400

    1. impl of dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+// to specify a default use the path '' in the array
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
 
 // you call for(root) to initialize the listening for navigation events on the root level of the app
 @NgModule({
