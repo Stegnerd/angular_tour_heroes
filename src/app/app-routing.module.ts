6200e801940188cd6f0a5a69e684f4f8633bc980
dstegner <dmstegner@gmail.com>
2020-08-16 13:16:02 -0400

    1. added basic routing 2. added dashboard skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HeroesComponent } from './heroes/heroes.component';
 
+const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+
+// you call for(root) to initialize the listening for navigation events on the root level of the app
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
